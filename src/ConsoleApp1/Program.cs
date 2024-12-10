b164211a17bd59959b84d53769c2a5b98c27ca56
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2024-12-10 16:57:56 +0900

    feat: add sample app

---
+++ src/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+Console.WriteLine($$"""
+{{nameof(ApplicationInformation.Current.AssemblyInformationalVersion)}} : {{ApplicationInformation.Current.AssemblyInformationalVersion}}
+{{nameof(ApplicationInformation.Current.FileVersion)}} : {{ApplicationInformation.Current.FileVersion}}
+{{nameof(ApplicationInformation.Current.ProductVersion)}} : {{ApplicationInformation.Current.ProductVersion}}
+""");
+
+public class ApplicationInformation
+{
+    public static ApplicationInformation Current { get; } = new ApplicationInformation();
+    public string? AssemblyInformationalVersion { get; } = typeof(ApplicationInformation).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+    public string? FileVersion { get; } = FileVersionInfo.GetVersionInfo(typeof(ApplicationInformation).Assembly.Location)?.FileVersion;
+    public string? ProductVersion { get; } = FileVersionInfo.GetVersionInfo(typeof(ApplicationInformation).Assembly.Location)?.ProductVersion;
+}
